2977b87b69a3c89e34bdf6769a988204d024d6a7
Artsiom Lipchyk <artsiom.lipchyk@innowise-group.com>
2021-11-24 12:36:58 +0300

    change project structure

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
+import {Grid} from './components/grid/Grid'
 
-const App = ({ columns, rows }) => {
-  const rootRef = useRef();
-  const [start, setStart] = useState(0);
-  const [table, setTable] = useState([])
-  
-  function getTopHeight() {
-    return 40 * start;
-  }
-  function getBottomHeight() {
-    return 40 * (table.length - (start + 10 + 1));
-  }
-
-  useEffect(() => {
-    function makeTableData(w, h) {
-      return new Array(h).fill(0).map((_, row) => {
-        const obj = {}
-        for(let i = 0; i < w; i++) {
-          obj[i] = i === 0 ? row : i
-        }
-        return obj;
-      });
-    }
-    const tableData = makeTableData(columns, rows)
-    setTable(tableData)
-  }, [])
-  
-  useEffect(() => {
-    function onScroll(e) {
-      setStart(
-        Math.floor(e.target.scrollTop / 40)
-      );
-    }
-    rootRef.current.addEventListener('scroll', onScroll);
-
-    return () => {
-      rootRef.current.removeEventListener('scroll', onScroll);
-    }
-  }, [table.length]);
-  
+const App = () => {
   return (
-    <div style={{ height: 40 * 10 + 1, overflow: 'auto' }} ref={rootRef}>
-    <div style={{ height: getTopHeight() }} />
-    <table>
-      <tbody>
-        {table.slice(start, start + 10 + 1).map((row, rowIndex) => (
-          <tr
-            style={{ height: 40 }}
-            key={start + rowIndex}
-          >{Object.values(row).map((text, colIndex) => (
-            <td key={start + '' + rowIndex + colIndex}>{text}</td>
-          ))}</tr>
-        ))}
-      </tbody>
-    </table>
-    <div style={{ height: getBottomHeight() }} />  
-    </div>
+    <>
+      <Grid 
+        columns={10}
+        rows={500}
+        rowHeight={40}
+      />
+    </>
   )
 }
 
---
+++ src/components/grid/Grid.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './Grid.css'
+
+export const Grid = ({ columns, rows, rowHeight }) => {
+  const rootRef = useRef(null);
+  const [start, setStart] = useState(0);
+  const [table, setTable] = useState([]);
+  const timeOut = 500
+  
+  function getTopHeight() {
+    return rowHeight * start;
+  }
+  function getBottomHeight() {
+    return rowHeight * (table.length - (start + 10 + 1));
+  }
+
+  useEffect(() => {
+    function makeTableData(w, h) {
+      return new Array(h).fill(0).map((_, row) => {
+        const obj = {}
+        for(let i = 0; i < w; i++) {
+          obj[i] = i === 0 ? row : i
+        }
+        return obj;
+      });
+    }
+    const tableData = makeTableData(columns, rows)
+    setTable(tableData)
+  }, [columns, rows])
+  
+  useEffect(() => {
+    function onScroll(e) {
+      setTimeout(() => {
+        setStart(
+          Math.floor(e.target.scrollTop / rowHeight)
+        );
+      }, timeOut);
+    }
+
+    rootRef.current.addEventListener('scroll', onScroll);
+
+    return () => {
+      rootRef.current.removeEventListener('scroll', onScroll);
+    }
+  }, [table.length, rowHeight, rootRef]);
+  
+  return (
+    <div style={{ height: rowHeight * 10 + 1, overflow: 'auto' }} ref={rootRef}>
+      <div style={{ height: getTopHeight() }} />
+      
+      <table>
+        <tbody>
+          {table.slice(start, start + 10 + 1).map((row, rowIndex) => (
+            <tr
+              style={{ height: rowHeight }}
+              key={start + rowIndex}
+            >{Object.values(row).map((text, colIndex) => (
+              <td key={start + '' + rowIndex + colIndex}>{text}</td>
+            ))}</tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div style={{ height: getBottomHeight() }} />  
+    </div>
+  )
+}
